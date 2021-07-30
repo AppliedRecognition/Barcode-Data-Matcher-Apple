0056ea429ce840748a9feead583763fb876d9f09
Jakub Dolejs <jakubdolejs@gmail.com>
2021-07-30 17:02:06 +0100

    Fixed dependency name

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BarcodeDataMatcher",
-            dependencies: ["AAMVABarcodeParser"]),
+            dependencies: ["AAMVA-Barcode-Parser-Apple"]),
         .testTarget(
             name: "BarcodeDataMatcherTests",
             dependencies: ["BarcodeDataMatcher"],
